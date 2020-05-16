455f586d7c66bd033887bcf1c5e793a69c184ba2
danniel.Vieira <danniel.hcvieira@gmail.com>
2020-05-16 12:36:19 -0300

    add autenticacao login

---
+++ js/fakedb_login.js
@@ -0,0 +1,14 @@
+var db_login = {
+    dados : [
+        {
+            email: 'mpsys@gmail.com',
+            senha: '1234',
+            username: 'MPSYS'
+        },
+        {
+            email: 'cliente@gmail.com',
+            senha: '1234',
+            username: 'cliente'
+        }
+    ]
+}
